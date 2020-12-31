a4465914ccb5281ba4a186816e31957ac8bfba3b
Ivan Valadez <ivan_valadez_11@hotmail.com>
2020-12-31 15:28:50 -0600

    Completing the ZTM Python tutorial

---
+++ Python/15_ML_DataScience/machine_learning.py
@@ -0,0 +1,12 @@
+"""
+1 - Import the data - https://www.kaggle.com/
+2 - Clean the data - Pandas/Numpy
+3 - Split data. Training Set/Test Set - Pandas/Numpy
+4 - Create a Model - Scikit-learn
+5 - Check the output - Scikit-learn/matplotlib
+6 - Improve
+
+Tutorials:
+https://elitedatascience.com/python-cheat-sheet
+https://github.com/GokuMohandas/madewithml/tree/4ad626098aca25db5628fe67895e738d5a5c2c2a
+"""
